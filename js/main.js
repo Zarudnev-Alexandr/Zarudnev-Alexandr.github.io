863b9c26ebf1a351b77f0e98a20c38435c1a7832
Zarudnev-Alexandr <alexandrzarudnev57@gmail.com>
2021-03-03 15:30:48 +0300

    change
    Да сменил сайт от греха подальше

--- js/main.js
+++ js/main.js
@@ -1,14 +1,63 @@
-$(function(){
-  $('.shop-items').slick({
-    dots: true,
-    prevArrow:false,
-    nextArrow: '<svg width="77" height="66" viewBox="0 0 77 66" fill="none" xmlns="http://www.w3.org/2000/svg"><g filter="url(#filter0_d)"><path d="M73 29C73 45.0163 57.5538 58 38.5 58C19.4462 58 4 45.0163 4 29C4 12.9837 19.4462 0 38.5 0C57.5538 0 73 12.9837 73 29Z" fill="#070707" fill-opacity="0.75"/></g><path d="M28 29.5085H50M50 29.5085L37.2812 19M50 29.5085L37.2812 39" stroke="white"/><defs><filter id="filter0_d" x="0" y="0" width="77" height="66" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/><feOffset dy="4"/><feGaussianBlur stdDeviation="2"/><feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.35 0"/><feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/><feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape"/></filter></defs></svg>'
+$(function () {
+
+  $('.menu-btn').on('click', function () {
+    $('.menu').toggleClass('menu--active'),
+      $('.firstslider').toggleClass('firstslider--active'),
+      $('.header-top__box').toggleClass('header-top__box--active'),
+      $('.header-title').toggleClass('header-title--active')
   });
 
-  $('.menu__btn').on('click', function(){
-    $('.menu-list').toggleClass('menu--active')
+  // $('.header-top__select-form').on('click', function(){
+  //   $('.firstslider').toggleClass('firstslider--active')
+  // })
+
+  $('.header-top__select').styler();
+
+  $('.firstslider-items').slick({
+    autoplay: true,
+    arrows: false,
+    dots: true,
+  });
+
+  $('.info-items').slick({
+    autoplay: true,
+    slidesToShow: 4,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+  $('.place-items').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    centerMode: true,
+    variableWidth: true,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 870,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
   })
-  $('.menu__btn').on('click', function(){
-    $('.menu__dop').toggleClass('menu__dop--active')
-  })
+
+  $('.connect-checkbox').styler();
 })
