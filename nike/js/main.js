09720504eb23ce89ab7b33f3090706f05bc24cb9
Zarudnev-Alexandr <alexandrzarudnev57@gmail.com>
2021-03-11 21:35:36 +0300

    added nike main and header adaptive
    Теперь появляется хоть что-то похожее на сайт

--- nike/js/main.js
+++ nike/js/main.js
@@ -6,5 +6,8 @@ $(function(){
     $('.header-drop').toggleClass('header-drop--active')
   });
 
-
+  $('.header-drop__item').on('click', function(){
+    $('.header-drop__item').removeClass('header-drop__item--active');
+    $(this).addClass('header-drop__item--active');
+  });
 })
