6c578dbbd08c6c995e10ea7b631a7cf5a9f92f87
Zarudnev-Alexandr <alexandrzarudnev57@gmail.com>
2021-03-14 19:35:07 +0300

    вроде убрал прокрутку на мобилках

--- nike/js/main.js
+++ nike/js/main.js
@@ -1,3 +1,6 @@
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 $(function(){
   
   $('.main-size').styler();
@@ -31,4 +34,17 @@ $(function(){
       },
     ]
   });
+
+
+
+  (function init100vh() {
+    function setHeight() {
+      var vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+    setHeight();
+    window.addEventListener('resize', setHeight);
+  })();
+
+
 })
