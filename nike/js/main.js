727b7ad5414817d3caac8c0842ccd4701c13d649
Zarudnev-Alexandr <alexandrzarudnev57@gmail.com>
2021-03-14 19:24:00 +0300

    added man
    Добавил и адаптировал секцию со слайдером

--- nike/js/main.js
+++ nike/js/main.js
@@ -2,6 +2,8 @@ $(function(){
   
   $('.main-size').styler();
 
+  $('.select').styler();
+
   $('.header-btn').on('click', function(){
     $('.header-drop').toggleClass('header-drop--active')
   });
@@ -9,5 +11,24 @@ $(function(){
   $('.header-drop__item').on('click', function(){
     $('.header-drop__item').removeClass('header-drop__item--active');
     $(this).addClass('header-drop__item--active');
+  });
+
+  $('.man-slider').slick({
+    Infinity:true,
+    slidesToShow:3,
+    slidesToScroll:1,
+    centerMode:true,
+    arrows:true,
+    prevArrow:'<svg class="arrow-prev" width="17" height="33" viewBox="0 0 17 33" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16 1L2 16.5L16 32" stroke="#ECECEC" stroke-width="2"/></svg>',
+    nextArrow:'<svg class="arrow-next" width="17" height="33" viewBox="0 0 17 33" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1L15 16.5L1 32" stroke="#ECECEC" stroke-width="2"/></svg>',
+    responsive: [
+      {
+        breakpoint: 650,
+        settings:{
+          // centerMode: false,
+          slidesToShow:1,
+        }
+      },
+    ]
   });
 })
